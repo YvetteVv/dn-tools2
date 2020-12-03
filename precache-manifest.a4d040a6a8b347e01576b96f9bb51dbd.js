df3774ec79f4744cf744a3fc07d9b6dcf0f50f5c
YvetteVv <yuwei@gwmail.gwu.edu>
2020-12-03 12:46:42 -0500

    Updates

---
+++ precache-manifest.a4d040a6a8b347e01576b96f9bb51dbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd14911129520527f93a",
+    "url": "./static/css/main.a05c8062.chunk.css"
+  },
+  {
+    "revision": "bd14911129520527f93a",
+    "url": "./static/js/main.3a87a87a.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.2c977d9b.js"
+  },
+  {
+    "revision": "4dd9176ee9d59db5da2e",
+    "url": "./static/css/2.ec653143.chunk.css"
+  },
+  {
+    "revision": "4dd9176ee9d59db5da2e",
+    "url": "./static/js/2.f3e639b2.chunk.js"
+  },
+  {
+    "revision": "a0ec80f6a512ad6b8d40f9ea44896f6c",
+    "url": "./index.html"
+  }
+];
