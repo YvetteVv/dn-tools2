b6e051397f5d32e587b218ff8a8bd06525326aa0
YvetteVv <yuwei@gwu.edu>
2020-12-07 15:54:39 -0500

    Updates

---
+++ precache-manifest.3c322d473f0112fd7f189b77d467a618.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e73168a0d942d3befcc",
+    "url": "./static/css/main.a28d50c8.chunk.css"
+  },
+  {
+    "revision": "4e73168a0d942d3befcc",
+    "url": "./static/js/main.fb7cbae5.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.2c977d9b.js"
+  },
+  {
+    "revision": "1a208774b1998382c7ac",
+    "url": "./static/css/2.ec653143.chunk.css"
+  },
+  {
+    "revision": "1a208774b1998382c7ac",
+    "url": "./static/js/2.d5c4eadf.chunk.js"
+  },
+  {
+    "revision": "8d396497d0728fcc769bd5954d28b243",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a776d745b54b926f02ce930779236acc.js"
+  "./precache-manifest.3c322d473f0112fd7f189b77d467a618.js"
 );
 
 workbox.clientsClaim();
